739e18c3773cef1251de6d833cdd50226e9ff862
vlapa <51048541+vlapa1973@users.noreply.github.com>
2023-05-20 23:25:51 +0300

    устранил косяк со второй HUM

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ NTPClient timeClient(ntpUDP, "ntp3.vniiftri.ru", utcOffsetInSeconds, utcPeriodMs
 
 const char *mqtt_server = "178.20.46.157";
 const uint16_t mqtt_port = 1883;
-const char *mqtt_client = "Tablo_Villa_001";
+const char *mqtt_client = "Tablo_Villa_002";
 const char *mqtt_client2 = "Villa_bme280_base";
 // const char *mqtt_client2 = "Home_bme280";
 const char *mqtt_client3 = "Villa_bme280_yama";
@@ -521,7 +521,7 @@ void bmeHumOut()
     t += outHumDataOut;
     z += "DDD" + t.substring(0, 2);
     color = colorHumOut;
-    flagHum = true;
+    flagHumOut = true;
 }
 
 //********************************************************************
